46015655e2c679473c37e78e9dc6012a6b97e753
rajat-12-jadon <136836017+rajat-12-jadon@users.noreply.github.com>
2024-11-03 18:18:51 +0530

    Create game_win.cpp

---
+++ game_win.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int n = nums.size();
+        int sum1 = 0, sum2 = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] < 10)
+            {
+                sum1 += nums[i];
+            }
+            else
+            {
+                sum2 += nums[i];
+            }
+        }
+        if(sum1 > sum2 || sum1 < sum2)
+        {
+            return true;
+        }
+        else
+        return false;
+    }
+};
