104844a15fe96c7a9f083afa13255e1c544d576a
rajat-12-jadon <136836017+rajat-12-jadon@users.noreply.github.com>
2024-11-18 15:02:18 +0530

    Create minCharsToAddForPalindrome.cpp

---
+++ minCharsToAddForPalindrome.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    int minChar(string& str) {
+        // Write your code here
+        string rev = str;
+        reverse(rev.begin(), rev.end());
+        int size = str.size();
+        str += '$';
+        str += rev;
+        int n = str.size();
+        
+        vector<int> lps(n, 0);
+        
+        int pre = 0, suf = 1;
+        
+        while(suf < str.size())
+        {
+            if(str[pre] == str[suf])
+            {
+                lps[suf] = pre + 1;
+                pre++, suf++;
+            }
+            else
+            {
+                if(pre == 0)
+                suf++;
+                else
+                {
+                    pre = lps[pre - 1];
+                }
+            }
+        }
+        return size - lps[n - 1];
+    }
+};
