d9f7f29395bb0b29d2a9b14fefd0d73657dfa7de
Rajat Jadon <136836017+rajat-12-jadon@users.noreply.github.com>
2025-01-11 23:31:35 +0530

    Create longestSubstringWithDistinctCharacters.cpp

---
+++ longestSubstringWithDistinctCharacters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int longestUniqueSubstr(string &s) {
+        // code here
+        unordered_map<char, int> charIndexMap;
+        int maxLength = 0;
+        int start = 0;
+        
+        for(int end = 0; end < s.length(); end++)
+        {
+            char currentChar = s[end];
+            if(charIndexMap.find(currentChar) != charIndexMap.end() && charIndexMap[currentChar] >= start)
+            {
+                start = charIndexMap[currentChar] + 1;
+            }
+            
+            charIndexMap[currentChar] = end;
+            
+            maxLength = max(maxLength, end - start + 1);
+        }
+        return maxLength;
+    }
+};
