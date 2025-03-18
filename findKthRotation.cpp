965dc1acc546741c8b64d572d0032252662fb921
Rajat Jadon <136836017+rajat-12-jadon@users.noreply.github.com>
2025-03-18 12:24:02 +0530

    Create findKthRotation.cpp

---
+++ findKthRotation.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        // Code Here
+        int low = 0, high = arr.size() - 1;
+        
+        while(low < high)
+        {
+            int mid = low + (high - low)/2;
+            if(arr[mid] > arr[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+        return low;
+    }
+};
