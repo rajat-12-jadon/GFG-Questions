c1673c508c936b07db7d0049adc0ba81d344c944
Rajat Jadon <136836017+rajat-12-jadon@users.noreply.github.com>
2025-04-20 04:22:57 +0530

    Create atLeastKOccurrences.cpp

---
+++ atLeastKOccurrences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int firstElementKTime(vector<int>& arr, int k) {
+        // write code
+        unordered_map<int, int> freq;
+        
+        for(int i = 0; i < arr.size(); i++)
+        {
+            freq[arr[i]]++;
+            
+            if (freq[arr[i]] == k) 
+            {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+};
